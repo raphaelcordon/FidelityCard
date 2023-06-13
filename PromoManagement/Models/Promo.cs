b7311a43bda2675fc88a91277dceae1ceeb91515
Raphael Cordon <cordon@outlook.com>
2023-06-13 11:04:25 +0200

    Models with Business rules of PromoManagement

--- PromoManagement/Models/Promo.cs
+++ PromoManagement/Models/Promo.cs
@@ -2,22 +2,43 @@
 
 namespace PromoManagement.Models
 {
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+
     public class Promo
     {
-        [Key]
-        [Required]
-        int Id { get; set; }
-        [Required]
-        [MaxLength(25)]
-        string Name { get; set; }
-        [Required]
-        [StringLength(300)]
-        string Description { get; set; }
-        [Required]
-        DateTime DateTimeStart { get; set; }
-        [Required]
-        DateTime DateTimeEnd { get; set; }
-        [Required]
-        int Fidelity { get; set; } = 0;
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageFileName { get; set; }
+        public int TimesToPrize { get; set; }
+        public DateTime DateTimeStart { get; set; }
+        public DateTime DateTimeEnd { get; set; }
+    }
+
+    public class UserPromoCard
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid PromoId { get; set; }
+        public int TimesToPrizeCount { get; set; }
+    }
+
+    public class UserCupon
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public bool Used { get; set; }
     }
 }
